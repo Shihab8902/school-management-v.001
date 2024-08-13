f6c90280d9c0ceca771ec4029607565d2673ca0e
shihab8902 <shihabbd018@gmail.com>
2024-08-14 04:51:41 +0600

    added form validation

--- client/src/Pages/Register/Register.tsx
+++ client/src/Pages/Register/Register.tsx
@@ -3,10 +3,14 @@ import { MdEmail, MdOutlineNumbers } from "react-icons/md";
 import { RiLockPasswordFill } from "react-icons/ri";
 import { IoEye } from "react-icons/io5";
 import { IoEyeOff } from "react-icons/io5";
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Toggler from '../../Components/Toggler/Toggler';
 import { FaUser } from 'react-icons/fa';
+import { UserContext } from '../../Context/AuthProvider';
+import { useForm, SubmitHandler } from "react-hook-form"
+
+
 
 
 const Register = () => {
@@ -16,15 +20,34 @@ const Register = () => {
     const [accountType, setAccountType] = useState('');
 
 
+    //Handle user registration
+
+    //Initial setups
+    const { user, createUser } = useContext(UserContext);
+    type Inputs = {
+        fullName: string
+        email: string,
+        accountType: string,
+        teacherID: string,
+        studentID: string,
+        password: string
+
+    }
+
+    const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>()
+
+    const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
+
+
+
+
 
 
 
     return <div className="min-h-screen   bg-white dark:bg-slate-900 lg:bg-gray-100 lg:dark:bg-[#18181B] flex justify-center items-center ">
 
-        <div className=" md:w-8/12  w-full bg-transparent grid min-h-screen lg:grid-cols-2 p-5 md:p-10" >
-            <div className="absolute border-2 p-2 border-gray-300 bg-gray-100 dark:border-gray-700 dark:bg-[#222831] rounded-md  z-10 md:hidden lg:block right-10 top-32" >
-                <Toggler size={24} />
-            </div>
+        <div className=" md:w-8/12  container w-full bg-transparent grid min-h-screen lg:grid-cols-2 p-5 md:p-10" >
+
             {/*Cover */}
             <div className="bg-[#008080] hidden lg:flex justify-center items-center rounded-tl-lg rounded-bl-lg shadow-lg shadow-[#00000012]">
                 {/* Logo */}
@@ -39,7 +62,9 @@ const Register = () => {
             {/* Form */}
             <div className="bg-transparent relative lg:bg-white lg:dark:bg-[#222831] rounded-tr-lg  lg:p-5 flex flex-col justify-center items-center rounded-br-lg lg:shadow-lg shadow-[#00000012] ">
 
-
+                <div className="absolute md:hidden lg:block right-1 top-1" >
+                    <Toggler size={24} />
+                </div>
                 <div className=' w-full relative md:bg-gray-100 md:dark:bg-gray-900 md:p-6 rounded-md '>
                     <div className='lg:hidden mb-6'>
                         <img src={logo} className='w-20 mx-auto' alt="Nolua Primary Government School" />
@@ -47,14 +72,14 @@ const Register = () => {
                     <div className="absolute hidden md:block lg:hidden right-4 top-4" >
                         <Toggler size={24} />
                     </div>
-                    <form >
+                    <form onSubmit={handleSubmit(onSubmit)}>
 
                         {/* Full name */}
                         <div className='mb-5'>
                             <label htmlFor="Name" className='font-semibold text-black dark:text-white text-sm mb-2 block'>Full Name</label>
                             <div className='flex items-center w-full bg-gray-200 dark:bg-gray-700 px-3  rounded-md '>
                                 <span ><FaUser className='text-xl text-gray-600 dark:text-white ' /></span>
-                                <input className='bg-transparent text-sm text-black dark:text-white font-medium placeholder:font-normal py-3 px-2  w-full outline-none' type="email" name="email" id="email" placeholder='example@email.com' />
+                                <input className='bg-transparent text-sm text-black dark:text-white font-medium placeholder:font-normal py-3 px-2  w-full outline-none' type="text" {...register("fullName", { required: true, minLength: 2, })} name="name" id="name" placeholder='Enter your full name' />
                             </div>
                         </div>
 
@@ -63,7 +88,7 @@ const Register = () => {
                             <label htmlFor="email" className='font-semibold text-black dark:text-white text-sm mb-2 block'>Email</label>
                             <div className='flex items-center w-full bg-gray-200 dark:bg-gray-700 px-3  rounded-md '>
                                 <span ><MdEmail className='text-xl text-gray-600 dark:text-white ' /></span>
-                                <input className='bg-transparent text-sm text-black dark:text-white font-medium placeholder:font-normal py-3 px-2  w-full outline-none' type="email" name="email" id="email" placeholder='example@email.com' />
+                                <input className='bg-transparent text-sm text-black dark:text-white font-medium placeholder:font-normal py-3 px-2  w-full outline-none' type="email" {...register("email", { required: true, pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/ })} name="email" id="email" placeholder='example@email.com' />
                             </div>
                         </div>
 
@@ -71,7 +96,7 @@ const Register = () => {
                         <div className='mb-5'>
                             <label htmlFor="accountType" className='font-semibold text-black dark:text-white text-sm mb-2 block'>Account Type</label>
                             <div className='flex items-center w-full bg-gray-200 dark:bg-gray-700   rounded-md '>
-                                <select className='bg-transparent dark:bg-gray-700 cursor-pointer text-sm rounded-md text-black dark:text-white font-medium placeholder:font-normal py-3 px-2  w-full outline-none' defaultValue="" onChange={(e) => setAccountType(e.target.value)} name="accountType" id="accountType">
+                                <select className='bg-transparent dark:bg-gray-700 cursor-pointer text-sm rounded-md text-black dark:text-white font-medium placeholder:font-normal py-3 px-2  w-full outline-none' defaultValue="" {...register("accountType", { required: true })} onChange={(e) => setAccountType(e.target.value)} name="accountType" id="accountType">
                                     <option value="" disabled>Select an account type</option>
                                     <option value="Teacher">Teacher</option>
                                     <option value="Student">Student</option>
@@ -86,7 +111,7 @@ const Register = () => {
                                 <label htmlFor="id" className='font-semibold text-black dark:text-white text-sm mb-2 block'>Teacher ID</label>
                                 <div className='flex items-center w-full bg-gray-200 dark:bg-gray-700 px-3 rounded-md '>
                                     <span ><MdOutlineNumbers className='text-xl text-gray-600 dark:text-white ' /></span>
-                                    <input className='bg-transparent text-sm text-black dark:text-white font-medium placeholder:font-normal py-3 px-2  w-full outline-none' type="text" name="teacherId" id="teacherId" placeholder='Enter teacher id' />
+                                    <input className='bg-transparent text-sm text-black dark:text-white font-medium placeholder:font-normal py-3 px-2  w-full outline-none' type="text" {...register("teacherID", { required: true })} name="teacherId" id="teacherId" placeholder='Enter teacher id' />
                                 </div>
                             </div>
 
@@ -100,7 +125,7 @@ const Register = () => {
                                 <label htmlFor="id" className='font-semibold text-black dark:text-white text-sm mb-2 block'>Student ID</label>
                                 <div className='flex items-center w-full bg-gray-200 dark:bg-gray-700 px-3 rounded-md '>
                                     <span ><MdOutlineNumbers className='text-xl text-gray-600 dark:text-white ' /></span>
-                                    <input className='bg-transparent text-sm text-black dark:text-white font-medium placeholder:font-normal py-3 px-2  w-full outline-none' type="text" name="studentId" id="studentId" placeholder='Enter student id' />
+                                    <input className='bg-transparent text-sm text-black dark:text-white font-medium placeholder:font-normal py-3 px-2  w-full outline-none' type="text" {...register("studentID", { required: true })} name="studentId" id="studentId" placeholder='Enter student id' />
                                 </div>
                             </div>
 
@@ -113,7 +138,7 @@ const Register = () => {
                             <label htmlFor="password" className='font-semibold text-black dark:text-white text-sm mb-2 block'>Password</label>
                             <div className='flex items-center w-full gap-2 bg-gray-200 dark:bg-gray-700 px-3  rounded-md '>
                                 <span ><RiLockPasswordFill className='text-xl text-gray-600 dark:text-white ' /></span>
-                                <input className='bg-transparent text-sm text-black dark:text-white font-medium placeholder:font-normal py-3  w-full outline-none' type={passwordVisible ? "text" : "password"} name="password" id="password" placeholder='Enter password' />
+                                <input className='bg-transparent text-sm text-black dark:text-white font-medium placeholder:font-normal py-3  w-full outline-none' type={passwordVisible ? "text" : "password"} {...register("password", { required: true, pattern: /^(?=.*[!@#$%^&*(),.?":{}|<>])[A-Za-z\d!@#$%^&*(),.?":{}|<>]{6,}$/ })} name="password" id="password" placeholder='Enter password' />
                                 <span onClick={() => setPasswordVisible(!passwordVisible)} className='text-xl text-gray-400 cursor-pointer' >{passwordVisible ? <IoEye /> : <IoEyeOff />}</span>
                             </div>
                         </div>
